0e9a54ee795dad37d2c25878e951d2637ca1dbee
EsraaMamoun <esraamamoun5@gmail.com>
2020-03-03 09:29:08 +0200

    commited

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 `use strict`;
 
 var productsImg = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
-
+var uniqueViews = [];
 
 var leftProductImg = document.querySelector('#leftProductImg');
 var centerProductImg = document.querySelector('#centerProductImg');
@@ -34,19 +34,23 @@ function render() {
     centerProduct = Product.all[randomNumber(0, Product.all.length - 1)];
     rightProduct = Product.all[randomNumber(0, Product.all.length - 1)];
 
-    while (leftProduct === centerProduct || leftProduct === rightProduct || centerProduct === rightProduct) {
+    while (leftProduct === centerProduct || leftProduct === rightProduct || centerProduct === rightProduct || uniqueViews.includes(leftProduct.pathOfImage) || uniqueViews.includes(centerProduct.pathOfImage) || uniqueViews.includes(rightProduct.pathOfImage)) {
         render();
     }
 
     leftProductImg.setAttribute('src', leftProduct.pathOfImage);
     leftProductImg.setAttribute('alt', leftProduct.productName);
     leftProductImg.setAttribute('title', leftProduct.productName);
+
     centerProductImg.setAttribute('src', centerProduct.pathOfImage);
     centerProductImg.setAttribute('alt', centerProduct.productName);
     centerProductImg.setAttribute('title', centerProduct.productName);
+
     rightProductImg.setAttribute('src', rightProduct.pathOfImage);
     rightProductImg.setAttribute('alt', rightProduct.productName);
     rightProductImg.setAttribute('title', rightProduct.productName);
+
+    uniqueViews = [leftProduct.pathOfImage,centerProduct.pathOfImage,rightProduct.pathOfImage];
 }
 
 render();
